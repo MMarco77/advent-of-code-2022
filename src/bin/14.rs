97be6aa6cba0d0a2c614704eb6d2112b8b4e4552
user <noname@antispam.com>
2022-12-15 00:44:41 +0100

    Fix to update README

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,10 +1,8 @@
-use advent_of_code::error::AppResult;
-
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(_input: &str) -> Option<u32> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
